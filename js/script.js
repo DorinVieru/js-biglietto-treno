476aed3578ada15367e814a24c4738636af4807d
DorinVieru <dorino.vieru@gmail.com>
2023-11-20 16:43:37 +0100

    [UPDATE] Create if condition for price

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ let cost = 0.21;
 let price;
 // let n = price.toFixed(2);
 
+// CALCOLO DEL PREZZO
+if (eta <= 18) {
+    let sconto = cost * 0.2 * km;
+    price = cost * km - sconto;
+
+    if ((eta >= 19) && (eta <= 64)) {
+        price = cost * km;
+    }
+
+}
+if (eta >= 65) {
+    sconto = cost * 0.4 * km;
+    price = cost * km - sconto;
+}
+
+// PREZZO DEL BIGLIETTO IN CONSOLE.LOG
+console.log(price);
