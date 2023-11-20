c64c5c0247adedb54419a3e1a48b961524e4ee55
DorinVieru <dorino.vieru@gmail.com>
2023-11-20 16:41:50 +0100

    [UPDATE] Create prompt, price & cost

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// PROMPT KM DA PERCORRERE
+let km = prompt ("Inserisci i km da percorrere");
+
+// PROMPT ETA
+let eta = prompt("Inserisci la tua età");
+
+// PREZZO BIGLIETTO
+let cost = 0.21;
+
+let price;
+// let n = price.toFixed(2);
+
